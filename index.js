da74ee62603cebe58c1b10d158dc1635c2282f02
ProfRedacted <113231286+ProfRedacted@users.noreply.github.com>
2023-10-24 15:27:51 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const express = require('express')
+const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+const port = 3000
+
+const loginRouter = require('./routes/login');
+const registerRouter = require('./routes/register');
+
+const app = express();
+
+app.use('/', loginRouter);
+app.use('/login', loginRouter);
+app.use('/register',registerRouter)
+
+app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
+
+app.engine('hbs',exphbs.engine({extname: '.hbs'}))
+app.set('view engine','hbs')
+app.set('views','./views')
+
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser:true,
+  useUnifiedTopology: true
+})
+
+const db = mongoose.connection
+db.on('error', error => console.log(error))
+db.once('open', () => console.log('Connected to moongoose'))
+
+const currPort = app.listen(process.env.PORT || port)
+console.log("Server now listening on port %d", currPort.address().port)
