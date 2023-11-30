ea7536796bc36b4a3716f8a48f0a63171e6d7cdd
Darius <darius.vincent5@gmail.com>
2023-12-01 07:32:13 +0800

    Solve memory leak on deployment

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import MongoStore from 'connect-mongo'; // Solve memory leak on deployment
 
 import passport from 'passport';
 import cookieParser from 'cookie-parser';
@@ -21,36 +22,6 @@ const session_secret = process.env.SESSION_SECRET || 'secret';
 
 const app = express();
 
-// Use Middleware
-app.use(express.json());
-app.use(cors({
-    origin: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type'],
-    credentials: true
-})); 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(session({
-    secret: session_secret,
-    resave: false,
-    saveUninitialized: false,
-    cookie: { 
-        maxAge: 8 * 180 * 60 * 1000,
-        httpOnly: true, // Helps mitigate certain types of attacks such as Cross-Site Scripting (XSS)
-        sameSite: 'strict', // Helps protect against Cross-Site Request Forgery (CSRF) attacks
-    },
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(cookieParser(session_secret));
-PassportConfig(passport);
-
-// Use Routers
-app.use("/", authRoute);
-app.use("/transactions", transactionsRoute);
-app.use("/users", userRoute);
-
 // Connect to MongoDB and listen to port
 if (mongodb_url) {
     mongoose.connect(mongodb_url).then(() => {
@@ -67,3 +38,34 @@ if (mongodb_url) {
 else {
     console.error("MongoDB URL is not defined.");
 }
+
+// Use Middleware
+app.use(express.json());
+app.use(cors({
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+    credentials: true
+})); 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+    secret: session_secret,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({client: mongoose.connection.getClient()}), // To solve memory leak on deployment
+    cookie: { 
+        maxAge: 8 * 180 * 60 * 1000,
+        httpOnly: true, // Helps mitigate certain types of attacks such as Cross-Site Scripting (XSS)
+        sameSite: 'strict', // Helps protect against Cross-Site Request Forgery (CSRF) attacks
+    },
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(cookieParser(session_secret));
+PassportConfig(passport);
+
+// Use Routers
+app.use("/", authRoute);
+app.use("/transactions", transactionsRoute);
+app.use("/users", userRoute);
