c97aa064b2d23ef293570f53e170ad84cce6bbf3
Darius <darius.vincent5@gmail.com>
2023-12-01 13:29:10 +0800

    just

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -3,14 +3,13 @@ import '../assets/css/Spinner.css';
 
 const Spinner = () => {
   return (
-<section className="dots-container">
-  <div className="dot"></div>
-  <div className="dot"></div>
-  <div className="dot"></div>
-  <div className="dot"></div>
-  <div className="dot"></div>
-</section>
-
+  <section className="dots-container">
+    <div className="dot"></div>
+    <div className="dot"></div>
+    <div className="dot"></div>
+    <div className="dot"></div>
+    <div className="dot"></div>
+  </section>
   )
 }
 
